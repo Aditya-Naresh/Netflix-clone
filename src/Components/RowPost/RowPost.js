85a83a532e8a7523588b0068ede8db47a34207b7
Aditya-Naresh <adityanaresh007@gmail.com>
2023-03-20 17:02:57 +0530

    TMDB api Integrated

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,9 +1,11 @@
 import axios from '../../axios';
 import React, { useEffect,useState } from 'react'
 import "./RowPost.css"
-import { imageUrl } from '../../constants/constants';
+import { imageUrl,API_KEY } from '../../constants/constants';
+import YouTube from 'react-youtube';
 function RowPost(props) {
   const [movies, setMovies] = useState([])
+  const [urlId, setUrlId] = useState()
   useEffect(() => {
      axios.get(props.url).then(response=>{
       console.log(response.data);
@@ -14,7 +16,25 @@ function RowPost(props) {
   
     
   }, [])
-  
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleMovie = (id) =>{
+    console.log(id);
+    axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response => {
+      if(response.data.results.length !==0){
+        setUrlId(response.data.results[0])
+      }else{
+        console.log('Empty Array');
+      }
+    })
+  }
   return (
     <div className='row'>
         <h2>{props.title}</h2>
@@ -22,12 +42,13 @@ function RowPost(props) {
 
 
           {movies.map((obj) => 
-            <img className={props.isSmall? 'smallPoster':'poster'} src= {`${imageUrl+obj.backdrop_path}`} alt="poster" />
+            <img onClick={()=>handleMovie(obj.id)} className={props.isSmall? 'smallPoster':'poster'} src= {`${imageUrl+obj.backdrop_path}`} alt="poster" />
 
           )}
             
             
         </div>
+        { urlId && <YouTube videoId={urlId.key} opts={opts}  /> }
     </div>
   )
 }
