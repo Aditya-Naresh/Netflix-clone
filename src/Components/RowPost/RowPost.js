b1998d6601778f3e0d6803d2b0fddf4955552415
Aditya-Naresh <adityanaresh007@gmail.com>
2023-04-03 13:16:58 +0530

    Bugs cleared . Moving on to add search bar

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,20 +1,20 @@
 import axios from '../../axios';
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./RowPost.css"
-import { imageUrl,API_KEY } from '../../constants/constants';
+import { imageUrl, API_KEY } from '../../constants/constants';
 import YouTube from 'react-youtube';
 function RowPost(props) {
   const [movies, setMovies] = useState([])
   const [urlId, setUrlId] = useState()
   useEffect(() => {
-     axios.get(props.url).then(response=>{
+    axios.get(props.url).then(response => {
       console.log(response.data);
       setMovies(response.data.results)
-     }).catch(err =>{
+    }).catch(err => {
       // alert('Network Error')
-     } )
-  
-    
+    })
+
+
   })
   const opts = {
     height: '390',
@@ -25,40 +25,40 @@ function RowPost(props) {
     },
   };
 
-  const handleMovie = (id) =>{
+  const handleMovie = (id) => {
     console.log(id);
     axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response => {
-      if(response.data.results.length !==0){
+      if (response.data.results.length !== 0) {
         setUrlId(response.data.results[0])
-      }else{
+      } else {
         console.log('Empty Array');
       }
     })
   }
   return (
     <div className='row'>
-        <h2>{props.title}</h2>
-        <div className="posters">
+      <h2>{props.title}</h2>
+      <div className="posters">
 
 
         {movies.map((obj) => (
-  <div key={obj.id} className="posterContainer">
-    <img
-      onClick={() => handleMovie(obj.id)}
-      className={props.isSmall ? 'smallPoster' : 'poster'}
-      src={`${imageUrl}${obj.backdrop_path}`}
-      alt="poster"
-    />
-    <div className="overlay">
-      <h4 className="title">{obj.title? obj.title:obj.name}</h4>
-    </div>
-  </div>
-))}
-           
-            
-        </div>
-        
-        { urlId && <YouTube videoId={urlId.key} opts={opts}  /> }
+          <div key={obj.id} className="posterContainer">
+            <img
+              onClick={() => handleMovie(obj.id)}
+              className={props.isSmall ? 'smallPoster' : 'poster'}
+              src={`${imageUrl}${obj.backdrop_path}`}
+              alt="poster"
+            />
+            <div className="overlay">
+              <h4 className="title">{obj.title ? obj.title : obj.name}</h4>
+            </div>
+          </div>
+        ))}
+
+
+      </div>
+
+      {urlId && <YouTube videoId={urlId.key} opts={opts} />}
     </div>
   )
 }
