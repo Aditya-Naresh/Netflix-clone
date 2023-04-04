76cdca558445379f2d85100d2993843ff6162646
Aditya-Naresh <adityanaresh007@gmail.com>
2023-04-04 16:02:08 +0530

    Search bar and the data fetching set up completed. Pending to add rendering of search result

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import { originals, TrendingMovies,ComedyMovies, HorrorMovies, ActionMovies,Roma
 import "./App.css"
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
-import SearchBar from "./Components/SearchBar/SearchBar";
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Banner/> 
-      <SearchBar/>
-      {/* <RowPost url={originals} title='Netflix Originals' />
+      
+      <RowPost url={originals} title='Netflix Originals' />
       <RowPost url={TrendingMovies} title='Trending' isSmall />
       <RowPost url={ComedyMovies} title='Comedy' isSmall />
       <RowPost url={HorrorMovies} title='Horror' isSmall />
       <RowPost url={ActionMovies} title='Action' isSmall />
       <RowPost url={RomanceMovies} title='Romance' isSmall />
-      <RowPost url={Documentaries} title='Documentaries' isSmall /> */}
+      <RowPost url={Documentaries} title='Documentaries' isSmall />
     </div>
   );
 }
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,37 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import {FaSearch} from "react-icons/fa";
-import "./SearchBar.css"
-import axios from 'axios';
-import { API_KEY } from '../../constants/constants';
-
+import React, { useEffect, useState, useRef } from "react";
+import { FaSearch } from "react-icons/fa";
+import "./SearchBar.css";
+import axios from "axios";
+import { API_KEY } from "../../constants/constants";
 
 function SearchBar() {
-    const [input, setInput] =useState();
-    const [nameList, setNameList] = useState([])
-    useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${input}`)
-        .then((response) => {setNameList(response.data.results);})
-    }, [])
+  const [input, setInput] = useState("");
+  const [nameList, setNameList] = useState([]);
+  const [selectedItem, setSelectedItem] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${input}`)
+      .then((response) => {
+        setNameList(response.data.results);
+      });
+  }, [input]);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      setShowDropdown(false);
+    }
+  };
+
+  const handleItemClick = (name) => {
+    setSelectedItem(name);
+    setInput("");
+    setShowDropdown(false);
+  };
+
+  const handleInputFocus = () => {
+    setShowDropdown(true);
+  };
+
+  const filteredList = nameList.filter((item) => {
+    if (input === "") {
+      return item;
+    } else if (item.title.toLowerCase().includes(input.toLowerCase())) {
+      return item;
+    }
+    return null;
+  });
+
   return (
-    <div className='input-wrapper'>
-        <FaSearch id="search-icon"/>
-        <input type="text" 
-        placeholder='Search Movies / Shows' 
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
+    <div className="input-wrapper" ref={wrapperRef}>
+      <div className="input">
+        <FaSearch id="search-icon" />
+        <input
+          type="text"
+          placeholder="Search Movies / Shows"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onFocus={handleInputFocus}
         />
-        {nameList.filter((item) => {
-            if(input === ""){
-                return item
-            }else if(item.title.toLowerCase().includes(input?.toLowerCase())){
-                console.log(item); 
-            }
-        }).map((item) => {
-            return item.name? item.name : item.title
-        })}
+      </div>
+      {showDropdown && filteredList.length > 0 && (
+        <ul className="dropdown">
+          {filteredList.map((item) => (
+            <li key={item.id} onClick={() => handleItemClick(item.name ? item.name : item.title)}>
+              {item.name ? item.name : item.title}
+            </li>
+          ))}
+        </ul>
+      )}
+      {selectedItem && (
+        <div className="selected-item">
+          <span>{selectedItem}</span>
+          <button onClick={() => setSelectedItem("")}>X</button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default SearchBar
-
+export default SearchBar;
